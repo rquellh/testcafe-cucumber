8939a5db67754c797bd568bc32dc76206769ebb2
Ryan Quellhorst <rquellhorst@hmbnet.com>
2018-03-27 13:58:44 -0400

    fixed promise structure

--- features/support/world.js
+++ features/support/world.js
@@ -3,14 +3,13 @@ const testControllerHolder = require('./testControllerHolder');
 var base64Img = require('base64-img');
 
 function CustomWorld({ attach, parameters }) {
-    this.waitForTestController = testControllerHolder.get()    
-    .then(function (tc) {
+    this.waitForTestController = testControllerHolder.get().then(function (tc) {
         return testController = tc;
-    })   
+    }) 
 
     this.attach = attach;
 
-    this.setBrowser = function() {
+    this.setBrowser = function () {
         if (parameters.browser === undefined) {
             return "chrome";
         } else {
@@ -19,15 +18,19 @@ function CustomWorld({ attach, parameters }) {
     }
 
     this.addScreenshotToReport = function () {
-        if (process.argv.includes("--format") || process.argv.includes("-f") || process.argv.includes("--format-options")){
-            return testController.takeScreenshot()
-            .then(function (buffer) {
-                var imgInBase64 = base64Img.base64Sync(buffer)
-                var imageConvertForCuc = imgInBase64.substring(imgInBase64.indexOf(",") + 1)
-                return attach(imageConvertForCuc, 'image/png');
-            })
+        if (process.argv.includes("--format") || process.argv.includes("-f") || process.argv.includes("--format-options")) {
+            testController.takeScreenshot()
+                .then(function (buffer) {
+                    var imgInBase64 = base64Img.base64Sync(buffer);
+                    var imageConvertForCuc = imgInBase64.substring(imgInBase64.indexOf(",") + 1);
+                    return attach(imageConvertForCuc, 'image/png');
+                })
+                .catch(function(){
+                })
+        } else {
+            return new Promise((resolve) => { resolve(null); });
         }
     }
 }
-
+ 
 setWorldConstructor(CustomWorld)
