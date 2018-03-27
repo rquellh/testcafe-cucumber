51f8e6eaa5fa2e2bb18e03fd0b6afdcd9008e0bc
Ryan Quellhorst <rquellhorst@hmbnet.com>
2018-03-27 13:56:41 -0400

    changed step definitions to async/await

--- features/step_definitions/github.js
+++ features/step_definitions/github.js
@@ -3,28 +3,28 @@ var Selector = require('testcafe').Selector;
 var Role = require('testcafe').Role;
 var githubPage = require('../support/pages/github-page');
 
-Given(/^I open the GitHub page$/, function () {
-    return testController.navigateTo(githubPage.github.url());
+Given(/^I open the GitHub page$/, async function () {
+    await testController.navigateTo(githubPage.github.url());
 });
 
-When(/^I am typing my search request "([^"]*)" on GitHub$/, function (text) {
-    return testController.typeText(githubPage.github.searchButton(), text)
+When(/^I am typing my search request "([^"]*)" on GitHub$/, async function (text) {
+    await testController.typeText(githubPage.github.searchButton(), text)
 });
 
-Then(/^I am pressing (.*) key on GitHub$/, function (text) {
-    return testController.pressKey(text);
+Then(/^I am pressing (.*) key on GitHub$/, async function (text) {
+    await testController.pressKey(text);
 });
 
-Then(/^I should see that the first GitHub\'s result is (.*)$/, function (text) {
-    return testController.expect(githubPage.github.firstSearchResult().innerText).contains(text);
+Then(/^I should see that the first GitHub\'s result is (.*)$/, async function (text) {
+    await testController.expect(githubPage.github.firstSearchResult().innerText).contains(text);
 });
 
-const gitHubRoleForExample = Role(githubPage.github.url() + "login", function (t) {
-    return t
+const gitHubRoleForExample = Role(githubPage.github.url() + "login", async function (t) {
+    await t
         .click(githubPage.github.loginButton())
         .expect(githubPage.github.loginErrorMessage().innerText).contains("Incorrect username or password.");
 });
 
-Then(/^I am trying to use (.*)$/, function (text) {
-    return testController.useRole(gitHubRoleForExample);
+Then(/^I am trying to use (.*)$/, async function (text) {
+    await testController.useRole(gitHubRoleForExample);
 });
--- features/step_definitions/google.js
+++ features/step_definitions/google.js
@@ -1,22 +1,22 @@
 var { Given, When, Then } = require('cucumber');
 var Selector = require('testcafe').Selector;
+const testcafe = require('testcafe');
 
-Given('I am open Google\'s search page', function () {
-    return testController.navigateTo('https://google.com');
+Given('I am open Google\'s search page', async function () {
+    await testController.navigateTo('https://google.com');
 });
 
-When('I am typing my search request {string} on Google', function (text) {
+When('I am typing my search request {string} on Google', async function (text) {
     var input = Selector('#lst-ib').with({ boundTestRun: testController });
-
-    return testController.typeText(input, text);
+    await this.addScreenshotToReport();
+    await testController.typeText(input, text);
 });
 
-Then('I press the {string} key on Google', async (text) => {
+Then('I press the {string} key on Google', async function (text) {
     await testController.pressKey(text);
 });
 
-Then('I should see that the first Google\'s result is {string}', function (text) {
+Then('I should see that the first Google\'s result is {string}', async function (text) {
     var firstLink = Selector('#rso').find('a').with({ boundTestRun: testController });
-
-    return testController.expect(firstLink.innerText).contains(text);
+    await testController.expect(firstLink.innerText).contains(text);
 });
